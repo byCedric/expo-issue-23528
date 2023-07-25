11d5a9fdbf9bb5ebd055911bc26e88c7c13886e8
Cedric van Putten <github@cedric.dev>
2023-07-25 21:37:42 +0200

    test

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,43 @@
-import { StyleSheet } from 'react-native';
+import { useCallback, useState } from 'react';
+import { Button, StyleSheet } from 'react-native';
+import { produce } from 'immer';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabTwoScreen() {
+  const [todos, setTodos] = useState([
+    {
+      id: "React",
+      title: "Learn React",
+      done: true
+    },
+    {
+      id: "Immer",
+      title: "Try Immer",
+      done: false
+    }
+  ]);
+
+  const handleAdd = useCallback(() => {
+    setTodos(
+      produce((draft) => {
+        draft.push({
+          id: "todo_" + Math.random(),
+          title: "A new todo",
+          done: false
+        });
+      })
+    );
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <Button title="Add" onPress={handleAdd} />
+      {todos.map((todo) => (
+        <Text key={todo.id}>{todo.title}</Text>
+      ))}
     </View>
   );
 }
